d922f38b7bb5cbb0630d2026d0c791b6acf9aca5
Home Desktop-WSL Ubuntu <26182964+ballooner@users.noreply.github.com>
2025-03-30 08:19:28 -0400

    Finished Chapter 7 Exercises

---
+++ chapter-7/exercises/exercise15.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * Use typedef to create types named Int8, Int16 and Int32. Define the types so that they represent 8-bit, 16-bit and 32-bit integers on your machine.
+ */
+
+typedef char Int8;
+typedef short Int16;
+typedef int Int32;
+
+int main(void)
+{
+    return 0;
+}
